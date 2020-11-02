90b8e3b5b66bd03b63258cc71eb0eb0332b3f2f2
peterztan <pt072093@gmail.com>
2020-11-01 19:08:54 -0600

    add mongoose, react skeleton and typescript

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Server has started!");
+    res.send("root");
 })
 
 const PORT = process.env.PORT || 80;
